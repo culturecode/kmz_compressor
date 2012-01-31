57a1a26e00fd062623d7c68628879dfe49c11b19
Ryan Wallace <rywall@gmail.com>
2012-01-31 15:26:08 -0800

    Save a hexencoded version of the kmz file in a cache directory.

--- lib/kmz_compressor/middleware.rb
+++ lib/kmz_compressor/middleware.rb
@@ -5,27 +5,28 @@ module KMZCompressor
     end
   
     def call(env)
-      @request = Rack::Request.new(env)
-      @cache_path = "public/#{@request.path_info}"
+      request = Rack::Request.new(env)
       
-      if @request.path_info.end_with? '.kmz'
-        @request.path_info = @request.path_info.gsub(/kmz\Z/, 'kml')
+      # If the User is asking for a KMZ file
+      if request.path_info.end_with? '.kmz'
+        # Use a hash of the request path (before we gsub it) as the filename we will save on the HD
+        cache_path = "public/kmz/#{Digest::SHA2.hexdigest(request.fullpath)}.kmz"
+        
+        # Ask Rails for KML instead of KMZ
+        request.path_info = request.path_info.gsub(/kmz\Z/, 'kml')
+        status, headers, response = @app.call(env)
+        
+        # Zip the KML response and save it on the HD
+        FileUtils.mkdir_p(File.dirname(cache_path))
+        kmz = Zippy.create(cache_path) do |zip| 
+          zip['doc.kml'] = response.body
+        end
 
-        @status, @headers, @response = @app.call(env)
-
-        [@status, @headers, self]
+        # Return the response to the next middleware in the chain
+        [status, headers, [kmz.data]]
       else
         @app.call(env)
       end
-    end
-
-    def each(&block)
-      FileUtils.mkdir_p(File.dirname(@cache_path))
-      kmz = Zippy.create(@cache_path) do |zip| 
-        zip['doc.kml'] = @response.body
-      end
-      
-      block.call(kmz.data)
     end
   end
 end
