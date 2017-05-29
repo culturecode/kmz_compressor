54158484fa661a8971811b9c17568c0ae6d6e73a
Ryan Wallace <rywall@gmail.com>
2017-05-29 11:17:07 -0400

    Support Rails 5.1
    Middleware name now needs to be a class, not a string.

--- lib/kmz_compressor/railtie.rb
+++ lib/kmz_compressor/railtie.rb
@@ -6,7 +6,7 @@ require 'kmz_compressor/middleware'
 module KMZCompressor
   class Railtie < Rails::Railtie
     initializer "kmz_compressor.init" do |app|
-      app.config.middleware.use "KMZCompressor::Middleware"
+      app.config.middleware.use KMZCompressor::Middleware
 
       Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
     end
