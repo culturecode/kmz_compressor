58b8e993eef0267711e96e878c0aa44d94898864
Nicholas Jakobsen <nicholas.jakobsen@gmail.com>
2017-09-08 20:36:43 -0700

    Remove helper that is not used in this project
    Other projects have more complicated implementations of this method, rendering this one unnecessary. It also ended up overriding a host app method, so is undesirable enough to remove.

--- app/helpers/kmz_compressor/kml_helper.rb
+++
@@ -1,8 +0,0 @@
-module KmzCompressor
-  module KmlHelper
-    # Converts KML colours which are BBGGRR into web colours which are RRGGBB
-    def kml_colour(html_hex_colour)
-      html_hex_colour.gsub(/([0-9A-F][0-9A-F])([0-9A-F][0-9A-F])([0-9A-F][0-9A-F])/i, '\3\2\1')
-    end
-  end
-end
