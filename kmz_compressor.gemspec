850e55a66c80a1cd686fdbf1cdbf574d9fb24d9a
Ryan Wallace <ryan@culturecode.ca>
2026-02-27 09:32:01 -0800

    Fix Google Earth Support.
    Google Earth Desktop doesn’t support Zip64.

--- kmz_compressor.gemspec
+++ kmz_compressor.gemspec
@@ -16,5 +16,5 @@ Gem::Specification.new do |s|
   s.files = Dir["{app}/**/*"] + Dir["{lib}/**/*"] + ["MIT-LICENSE", "README.rdoc"]
 
   s.add_dependency "rails", ">= 4.2"
-  s.add_dependency "rubyzip", ">= 1.0.0"
+  s.add_dependency "rubyzip", ">= 3.0.0"
 end
--- lib/kmz_compressor/railtie.rb
+++ lib/kmz_compressor/railtie.rb
@@ -6,6 +6,9 @@ require 'kmz_compressor/middleware'
 module KMZCompressor
   class Railtie < Rails::Railtie
     initializer "kmz_compressor.init" do |app|
+      # Google Earth Desktop doesn't support Zip64, so we need to disable it in rubyzip
+      Zip.write_zip64_support = false
+
       app.config.middleware.use KMZCompressor::Middleware
 
       Mime::Type.register "application/vnd.google-earth.kml+xml", :kml
